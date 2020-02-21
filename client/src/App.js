d6566c160772dcceecdecba1a0412de1bc583f0e
Michelle Quinn <michelle.n.quinn@gmail.com>
2020-02-21 14:02:03 -0600

    setting up the search profile and favorites routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        {/* <Navbar /> */}
+        {/* <Wrapper> */}
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/favorites" component={Favorites} />
+        {/* </Wrapper> */}
+        {/* <Footer /> */}
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
-
 
 export default App;
---
+++ client/src/utils/API.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the yelp API
