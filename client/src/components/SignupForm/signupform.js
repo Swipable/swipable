5e9dbc4130dc2eece00170b19bbe1be76e128d31
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-02-22 11:51:13 -0600

    update signup form -changed footer css to fixed

--- client/src/components/SignupForm/signupform.js
+++ client/src/components/SignupForm/signupform.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-function Card() {
+function SignupForm() {
     return (
         <div>
         <h5 class="card-title text-center">Register</h5>
         <form class="form-signin">
+            <div class="form-label-group">
+                    <input type="text" id="inputFirstName" class="form-control" />
+                    <label for="inputFirstName">First Name</label>
+                </div>
+
+            <div class="form-label-group">
+                <input type="text" id="inputZipCode" class="form-control"  />
+                <label for="inputZipCode">Last Name</label>
+            </div>
             <div class="form-label-group">
                 <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus />
                 <label for="inputUserame">Username</label>
@@ -13,7 +22,11 @@ function Card() {
                 <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required />
                 <label for="inputEmail">Email address</label>
             </div>
-
+            <div class="form-label-group">
+                <input type="text" id="inputZipCode" class="form-control"  />
+                <label for="inputZipCode">Zip Code</label>
+            </div>
+            
             <hr></hr>
 
             <div class="form-label-group">
@@ -26,8 +39,8 @@ function Card() {
                 <label for="inputConfirmPassword">Confirm password</label>
             </div>
 
-            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Register</button>
-            <a class="d-block text-center mt-2 small" href="#">Sign In</a>
+            <button id = 'registerBtn' class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Register</button>
+            <a class="d-block text-center mt-2 small" href="http://localhost:3000/login">Sign In</a> 
             <hr class="my-4"></hr>
             <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign up with Google</button>
             <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign up with Facebook</button>
@@ -36,4 +49,4 @@ function Card() {
 );
 }
 
-export default Card;
+export default SignupForm;
