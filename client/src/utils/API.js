a3c428c82608b7942a58bed3aee7f28c1f6750c9
Michelle Quinn <michelle.n.quinn@gmail.com>
2020-02-22 12:11:33 -0600

    adding axios api call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,8 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the yelp API
+export default axios.create({
+  baseURL: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3',
+  headers: {
+    Authorization: `Bearer "Y9M86BBstf-ATStxC6Y9r0Tq-0A1JDp5xGMAFki4cFeW8TgBeznAmQtOuFAPqtAgZsEnI2GfBQPJ4FelB7hyc3Ovb4DDVgoUgajUXr0NTHpydudR54iU2gm0AkJMXnYx"`
+  }
+});
