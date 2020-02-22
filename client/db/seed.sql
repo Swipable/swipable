701801a5eeb4abc9e301dae692094a1fb253df39
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-02-22 11:05:01 -0600

    add env, setup db schema

---
+++ client/db/seed.sql
@@ -0,0 +1,7 @@
+USE swipable_db;
+
+INSERT INTO Users (first_name, last_name, username, email, password, zip_code)
+VALUES ('test', 'test', 'test', 'test@test.com', 'test', 60661);
+
+INSERT INTO Users (first_name, last_name, username, email, password, zip_code)
+VALUES ('albus', 'dumbledor', 'adumbledor', 'headmaster@hogwarts.com', 'albus', 34654);
