bbbd669e5b9c490514d504889bd071dbcaac1a93
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-02-24 12:22:13 -0600

    add sequelize models and sequelize connection

--- client/db/config/database.js
+++ client/db/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('swipable_db', 'root', 'DebbieDoo', {
+const sequelize = new Sequelize('swipable_db', 'root', '', {
     host: "localhost",
     port: 3306,
     dialect: 'mysql',
