43617ba4eb38f06c46d8a44381eea9e4a2869aaf
Michelle Quinn <michelle.n.quinn@gmail.com>
2020-02-24 21:49:13 -0600

    making the api call return 50 businesses

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,7 @@
  app.get("/api/restaurants", (req, res) => {
      console.log("Hitting the correct route.");
      axios
-         .get("https://api.yelp.com/v3/businesses/search?location=chicago", 
+         .get("https://api.yelp.com/v3/businesses/search?location=chicago&limit=50", 
          { 'headers': { 'Authorization': 'Bearer Y9M86BBstf-ATStxC6Y9r0Tq-0A1JDp5xGMAFki4cFeW8TgBeznAmQtOuFAPqtAgZsEnI2GfBQPJ4FelB7hyc3Ovb4DDVgoUgajUXr0NTHpydudR54iU2gm0AkJMXnYx',
         'Access-Control-Allow-Origin': '*'} } )
         .then((response) => {
