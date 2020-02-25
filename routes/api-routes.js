f12da1dc2cbd93ada528a0119a19d25f971ee181
Michelle Quinn <michelle.n.quinn@gmail.com>
2020-02-24 21:23:21 -0600

    streamlining api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,21 @@
-// const axios = require('axios');
+ const axios = require('axios');
 
-// app.get("/api/restaurants", (req, res) => {
-//     console.log("Hitting the correct route.");
-//     axios
-//         .get("https://api.yelp.com/v3/businesses/search?location=chicago", 
-//         { 'headers': { 'Authorization': 'Bearer Y9M86BBstf-ATStxC6Y9r0Tq-0A1JDp5xGMAFki4cFeW8TgBeznAmQtOuFAPqtAgZsEnI2GfBQPJ4FelB7hyc3Ovb4DDVgoUgajUXr0NTHpydudR54iU2gm0AkJMXnYx',
-//        'Access-Control-Allow-Origin': '*'} } )
-//        .then((response) => {
-//         console.log("Yelp response received");
-//         res.json(response.data);
-//         // console.log(response.data)
-//        }).catch(e => console.log(e));
-//     // res.json(response)
-//   });
+ module.exports = function(app) {
+    
+ app.get("/api/restaurants", (req, res) => {
+     console.log("Hitting the correct route.");
+     axios
+         .get("https://api.yelp.com/v3/businesses/search?location=chicago", 
+         { 'headers': { 'Authorization': 'Bearer Y9M86BBstf-ATStxC6Y9r0Tq-0A1JDp5xGMAFki4cFeW8TgBeznAmQtOuFAPqtAgZsEnI2GfBQPJ4FelB7hyc3Ovb4DDVgoUgajUXr0NTHpydudR54iU2gm0AkJMXnYx',
+        'Access-Control-Allow-Origin': '*'} } )
+        .then((response) => {
+         console.log("Yelp response received");
+         res.json(response.data);
+        //   console.log(response.data)
+        }).catch(e => console.log(e));
+   });
 
-//   module.exports = APIroutes;
+//    app.get("/stuff", (req, res) => {
+//        console.log("Stuff received")
+//    })
+}
