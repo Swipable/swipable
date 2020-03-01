ea92fa0e77a764bdcb14655cd21f844f871587f6
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-03-01 17:54:44 -0600

    readd changes - posts to DB

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,44 +3,33 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the random user API
 export default {
   fetchRestaurants: function() {
-    return axios.get("api/restaurants").then(res => {
-      const restaurants = res.data.businesses;
-      return restaurants.map(restaurant => {
-        return {
-          name: restaurant.name,
-          rating: restaurant.rating,
-          price: restaurant.price,
-          image: restaurant.image_url,
-          link: restaurant.url,
-          closed: restaurant.is_closed,
-          id: restaurant.id,
-          display_phone: restaurant.phone,
-          display_address: restaurant.location.display_address,
-          catagory: restaurant.catagories
-        };
-      });
-    });
-  },
-  fetchUsers: function() {
     return axios
-      .get("/api/get/otheruserprofilefromdb", {
-        // params: { username: username }
-      })
+      .get("api/restaurants")
       .then(res => {
-        console.log("Res.data");
-        console.log(res.data);
-        const users = res.data;
-        return users.map(user => {
+        const restaurants = res.data.businesses;
+        return restaurants.map(restaurant => {
           return {
-            id: user.id,
-            first_name: user.first_name,
-            last_name: user.last_name,
-            username: user.username
+            name: restaurant.name,
+            rating: restaurant.rating,
+            price: restaurant.price,
+            image: restaurant.image_url,
+            link: restaurant.url,
+            closed: restaurant.is_closed,
+            id: restaurant.id,
+            display_phone: restaurant.phone,
+            display_address: restaurant.location.display_address,
+            catagory: restaurant.catagories,
           };
         });
-      })
-      .catch(function(error) {
-        console.log(error);
       });
+  },
+
+  saveUser:(userData) => {
+    return axios.post('/signup', userData);
+  },
+  getUsers:() => {
+    return axios.get('/users')
   }
+
+
 };
