cf8db40901a8dd0065f408b7d7fefc01c479e874
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-03-01 16:56:25 -0600

    moving routes into folder

---
+++ routes/api/api-routes.js
@@ -0,0 +1,55 @@
+const axios = require("axios");
+var db = require("../../models");
+
+module.exports = function(app) {
+  app.get("/api/restaurants", (req, res) => {
+    console.log("Hitting the correct route.");
+    axios
+      .get(
+        "https://api.yelp.com/v3/businesses/search?location=chicago&limit=50",
+        {
+          headers: {
+            Authorization:
+              "Bearer Y9M86BBstf-ATStxC6Y9r0Tq-0A1JDp5xGMAFki4cFeW8TgBeznAmQtOuFAPqtAgZsEnI2GfBQPJ4FelB7hyc3Ovb4DDVgoUgajUXr0NTHpydudR54iU2gm0AkJMXnYx",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      )
+      .then(response => {
+        console.log("Yelp response received");
+        res.json(response.data);
+        //   console.log(response.data)
+      })
+      .catch(e => console.log(e));
+  });
+
+  //    app.get("/stuff", (req, res) => {
+  //        console.log("Stuff received")
+  //    })
+
+  app.get("/api/get/otheruserprofilefromdb", function(req, res) {
+    db.Users.findAll({}).then(function(data) {
+      console.log(data);
+      res.json(data);
+      // res.render("team", { users: data });
+    });
+  });
+
+  app.get("/api/get/userprofile", function(req, res) {
+    db.Users.findOne({
+      where: {
+        username: "adumbledor"
+      }
+    }).then(function(data) {
+      console.log(data);
+      res.json(data);
+      // res.render("team", { users: data });
+    });
+  });
+  // axios.get('/api/get/otheruserprofilefromdb',
+  // {params: {username: username}} )
+  // .then(res =>  setProfile({...res.data} ))
+  //  .catch(function (error) {
+  //  console.log(error);
+  // })
+};
---
+++ routes/api/users-routes.js
@@ -0,0 +1,111 @@
+const express = require('express').Router();
+const usersController = require('../../controllers/usersController');
+//const Users = require ('../../models/Users');
+const db = require('../../models');
+
+/*
+  USER ROUTES
+*/
+
+module.exports = function(app) {
+    // app.get('/api/get/signup', (req, res, next) => {
+    //     console.log('get method for signup')
+    //     res.json(data);
+    // });
+
+app.route('/signup')
+    .post(usersController.create)
+    .get(usersController.findAll)
+
+
+
+
+/*
+  RESTAURANT API CALLS
+*/
+
+    app.get("/api/restaurants", (req, res) => {
+        console.log("Hitting the correct route.");
+        axios
+            .get("https://api.yelp.com/v3/businesses/search?location=chicago&limit=50", 
+            { 'headers': { 'Authorization': 'Bearer Y9M86BBstf-ATStxC6Y9r0Tq-0A1JDp5xGMAFki4cFeW8TgBeznAmQtOuFAPqtAgZsEnI2GfBQPJ4FelB7hyc3Ovb4DDVgoUgajUXr0NTHpydudR54iU2gm0AkJMXnYx',
+           'Access-Control-Allow-Origin': '*'} } )
+           .then((response) => {
+            console.log("Yelp response received");
+            res.json(response.data);
+           //   console.log(response.data)
+           }).catch(e => console.log(e));
+      });
+   
+   //    app.get("/stuff", (req, res) => {
+   //        console.log("Stuff received")
+   //    })
+   }
+
+
+/*
+
+    app.post('/api/signup', (req, res) => {
+        db.Users.create({
+            first_name: req.body.inputFirstName,
+            last_name: req.body.inputLastName,
+            username: req.body.inputUsername,
+            email: req.body.inputEmail,
+            password: req.body.inputPassword,
+            zip_code: req.body.inputZipCode
+        })
+        .then(user => {
+            console.log('signup werk')
+            console.log(user)
+            res.json(user)
+        })
+        .catch(error => {
+            console.log(error)
+          //  res.redirect('/signup');
+        });
+    });
+
+
+
+
+router.route('/')
+    .get(usersController.test)
+    .get(usersController.findAll)
+    .post(usersController.create)
+    */
+
+// route for user signup
+// router
+//     .get('/signup', (req, res, next) => {
+//         console.log('get method for signup')
+//         // next()
+//     })
+//     .post('/signup', (req, res) => {
+//         Users.create({
+//             first_name: req.body.inputFirstName,
+//             last_name: req.body.inputLastName,
+//             username: req.body.inputUsername,
+//             email: req.body.inputEmail,
+//             password: req.body.inputPassword,
+//             zip_code: req.body.inputZipCode
+//         })
+//         .then(user => {
+//             console.log('signup werk')
+//             console.log(user)
+//         })
+//         .catch(error => {
+//             console.log(error)
+//             res.redirect('/signup');
+//         });
+//     });
+
+
+// //PUT to update user's profile
+
+// //POST method to .findOne wer where username: username
+
+// //GET logout to stop session
+
+// //GET all data for one user
+
+// module.exports = router;
