1f7135b19002f9a60f9a141e85036eb40b343d0d
Michelle Quinn <michelle.n.quinn@gmail.com>
2020-03-07 12:01:05 -0600

    navbar update

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,21 +1,49 @@
 import React from "react";
 import "./navbar.css";
 
+
 function Navbar() {
   return (
-    <div className="header">
-      <div className="row">
-      <div className="col-lg-6">Icon</div>
-      <div className="col-lg-6">
-        <a href="/profile" className="link">Profile</a>
-        <a href="/search" className="link">Home</a>
-        <a href="/team" className="link">Team</a>
-        <a href="/favorites" className="link">Favourites</a>
-        <a href="/newsfeed" className="link">Newsfeed</a>
-        <a href="/login" className="link">Logout</a></div>
-      </div>
-    </div>
+<>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <a class="navbar-brand" href="#">
+    <img src="/logo.png" width="30" height="30" alt="cravings"/>
+    </a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/search">Search <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/profile">Profile <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/favorites">Favorites <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/newsfeed">Newsfeed <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+
+    <form class="form-inline my-2 my-lg-0">
+      <a href="logout"><button class="btn btn-outline-primary my-2 my-sm-0" >Logout</button></a>
+    </form>
+   
+  </div>
+</nav>
+
+</>
+
   );
+
 }
 
 export default Navbar;
+
