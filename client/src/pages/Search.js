0caaed67da4f2d5d420ac47b0f9929b0c76737e1
Michelle Quinn <michelle.n.quinn@gmail.com>
2020-03-09 19:42:02 -0500

    fixing RestaurantIndex

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -38,15 +38,15 @@ function Search() {
     // Ensure that the restaurant index stays within our range of restaurants
     if (restaurantIndex <= 49) {
       axios
-        .post("/api/post/favoritestodb", restaurants[restaurantIndex - 1])
+        .post("/api/post/favoritestodb", restaurants[restaurantIndex -1])
         .then(res =>
           alert(res.data.favorite.name + " has been added to your favorites <3")
         )
         .then(setRestaurant(restaurants[restaurantIndex]))
-        .then(setRestaurantIndex(restaurantIndex + 1));
+        .then(setRestaurantIndex(restaurantIndex));
     } else {
       axios
-        .post("/api/post/favoritestodb", restaurants[restaurantIndex - 1])
+        .post("/api/post/favoritestodb", restaurants[restaurantIndex -1])
         .then(res =>
           alert(res.data.favorite.name + " has been added to your favorites <3")
         )
@@ -58,6 +58,7 @@ function Search() {
     if (restaurantIndex <= 49) {
       setRestaurant(restaurants[restaurantIndex]);
       setRestaurantIndex(restaurantIndex);
+      console.log(restaurantIndex);
     } else {
       alert("There are no more results! Please refine your search.");
     }
@@ -67,11 +68,11 @@ function Search() {
     // Get the title of the clicked button
     const btnName = event.target.getAttribute("data-value");
     if (btnName === "next") {
-      const newRestaurantIndex = restaurantIndex + 1;
+      const newRestaurantIndex = restaurantIndex +1;
       nextRestaurant(newRestaurantIndex);
       console.log(restaurantIndex);
     } else {
-      const newRestaurantIndex = restaurantIndex + 1;
+      const newRestaurantIndex = restaurantIndex +1;
       dislikeRestaurant(newRestaurantIndex);
     }
   };
